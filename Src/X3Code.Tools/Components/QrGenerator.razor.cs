b36e5f2ab690b8847d08b5129584b55d521c4bbb
Clemens Mattner <cl.mattner@gmail.com>
2024-10-16 10:45:54 +0200

    Delete Counter page and update NavMenu links
    Removed the Counter page and its associated code. Updated the navigation menu to reflect this change by removing the counter link and streamlining the QR Code Generator link.

--- Src/X3Code.Tools/Components/QrGenerator.razor.cs
+++ Src/X3Code.Tools/Components/QrGenerator.razor.cs
@@ -1,7 +1,21 @@
 using Microsoft.AspNetCore.Components;
+using QRCoder;
 
 namespace X3Code.Tools.Components;
 
+//Source: https://github.com/codebude/QRCoder
 public partial class QrGenerator : ComponentBase
 {
+    private string? InputText { get; set; }
+    private string? QrCodeAsBase64 { get; set; }
+
+    private void CreateQrCode(string? content)
+    {
+        if (string.IsNullOrEmpty(content)) return;
+        
+        var qrGenerator = new QRCodeGenerator();
+        var qrCodeData = qrGenerator.CreateQrCode(content, QRCodeGenerator.ECCLevel.Q);
+        var qrCode = new Base64QRCode(qrCodeData);
+        QrCodeAsBase64 = qrCode.GetGraphic(20);
+    }
 }
--- Src/X3Code.Tools/Pages/Qr.razor.cs
+++ Src/X3Code.Tools/Pages/Qr.razor.cs
@@ -1,21 +1,7 @@
 using Microsoft.AspNetCore.Components;
-using QRCoder;
 
 namespace X3Code.Tools.Pages;
 
-//Source: https://github.com/codebude/QRCoder
 public partial class Qr : ComponentBase
 {
-    public string? InputText { get; set; }
-    public string? QrCodeAsBase64 { get; set; }
-
-    private void CreateQrCode(string? content)
-    {
-        if (string.IsNullOrEmpty(content)) return;
-        
-        var qrGenerator = new QRCodeGenerator();
-        var qrCodeData = qrGenerator.CreateQrCode(content, QRCodeGenerator.ECCLevel.Q);
-        var qrCode = new Base64QRCode(qrCodeData);
-        QrCodeAsBase64 = qrCode.GetGraphic(20);
-    }
 }

ff6283a38af2cc740caa74272962bfe43da2766c
Clemens Mattner <cl.mattner@gmail.com>
2024-10-16 12:05:38 +0200

    Update QR generator UI and add reset functionality
    Refactored the input and button elements to use MudBlazor components for a more modern look. Added a clear button to reset the input field and QR code, and improved input handling by adding an input placeholder.

--- Src/X3Code.Tools/Components/QrGenerator.razor.cs
+++ Src/X3Code.Tools/Components/QrGenerator.razor.cs
@@ -7,19 +7,25 @@ namespace X3Code.Tools.Components;
 //Source: https://github.com/codebude/QRCoder
 public partial class QrGenerator : ComponentBase
 {
+    private const string InputPlaceHolder = "Enter up to 250 characters";
     private string? InputText { get; set; }
     private string? QrCodeAsBase64 { get; set; }
+    private bool IsButtonDisabled { get; set; } = true;
 
     private void CreateQrCode(string? content)
     {
-        if (string.IsNullOrEmpty(content)) return;
+        if (string.IsNullOrEmpty(content))
+        {
+            QrCodeAsBase64 = null;
+            return;
+        }
         
         var qrGenerator = new QRCodeGenerator();
         var qrCodeData = qrGenerator.CreateQrCode(content, QRCodeGenerator.ECCLevel.Q);
         var qrCode = new Base64QRCode(qrCodeData);
         QrCodeAsBase64 = qrCode.GetGraphic(20);
     }
-    
+
     private void CheckEnter(KeyboardEventArgs e)
     {
         if (e.Key == "Enter" && !string.IsNullOrWhiteSpace(InputText))
@@ -28,9 +34,9 @@ public partial class QrGenerator : ComponentBase
         }
     }
 
-    private void OnInputChange(ChangeEventArgs e)
+    private void Clear()
     {
-        if (e.Value != null)
-            InputText = e.Value.ToString();
+        InputText = null;
+        QrCodeAsBase64 = null;
     }
 }
